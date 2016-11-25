de49fba447550ecf67730da20e6c45247a403bff
Zach Deibert <zachdeibert@gmail.com>
2016-11-25 10:18:24 -0600

    Added composer installer

--- compile.sh
+++ compile.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 set -e
 
+# Install composer
+curl -0 composer-setup.php https://getcomposer.org/installer
+if [ "$(sha384sum composer-setup.php | cut -d" " -f1)" != "$(curl https://composer.github.io/installer.sig)" ]; then
+    echo "Invalid signature"
+    exit 1
+fi
+php composer-setup.php --install-dir=bin --filename=composer
+rm composer-setup.php
+
 # Get the composer dependencies
 cd src
-composer install
+../bin/composer install
 cd ..
