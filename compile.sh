2ddd9867091a0d5978971d28776173015ccd75dc
Zach Deibert <zachdeibert@gmail.com>
2016-11-25 10:26:08 -0600

    Changed ci language

--- compile.sh
+++ compile.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 set -e
 
-# Install composer
-curl -o composer-setup.php https://getcomposer.org/installer
-if [ "$(sha384sum composer-setup.php | cut -d" " -f1)" != "$(curl https://composer.github.io/installer.sig)" ]; then
-    echo "Invalid signature"
-    exit 1
-fi
-php composer-setup.php --install-dir=bin --filename=composer
-rm composer-setup.php
-
 # Get the composer dependencies
 cd src
-../bin/composer install
+composer install
 cd ..
